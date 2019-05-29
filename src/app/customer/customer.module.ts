5e8ed9968d6c7dde144a9f5a52f94c4248a130b9
Ershad <ershad@gmail.com>
2019-05-30 05:33:58 +0600

    polishing

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,11 +1,8 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {SharedModule} from '../shared/module/shared.module';
-import {CustomerListComponent} from './customer-list/customer-list.component';
 
 import {CustomerRoutingModule} from './customer-routing.module';
-import {CustomerSaveComponent} from './customer-save/customer-save.component';
-import {CustomerComponent} from './customer/customer.component';
 
 @NgModule({
   declarations: [],
